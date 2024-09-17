57638ab296e6a9212abfaf59c596b91fb0384a83
Alice <aliceblack@westernwyoming.edu>
2024-09-16 21:05:27 -0700

    Update main.py

--- Hot-Dog-Cookout-Calculator/main.py
+++ Hot-Dog-Cookout-Calculator/main.py
@@ -1,7 +1,27 @@
-#
-# Name
-# Date
+
+# Alice J Black
+# 2024SEP16
 # Hot Dog Cookout Calculator Programming Project
-# COSC 2409 DNT
+# COSC1010
 #
-# Use comments liberally throughout the program. 
+# 
+
+# enter amount of people
+number_of_people = int(input("Enter number of people: "))
+
+# hotdogs per person
+number_of_hotdogs_per_person = int(input("Enter number of hot dogs: "))
+number_of_hotdogs_per_package = 10
+number_of_buns_per_package = 8
+
+# total number of hotdogs
+total_number_of_hotdogs = number_of_people * number_of_hotdogs_per_person
+number_of_hotdog_packages_needed = total_number_of_hotdogs // number_of_hotdogs_per_package
+number_of_hotdog_buns_needed = total_number_of_hotdogs // number_of_buns_per_package
+number_of_hotdogs_left_over = total_number_of_hotdogs % number_of_hotdogs_per_package
+number_of_hotdog_buns_left_over = total_number_of_hotdogs % number_of_buns_per_package
+
+print("Minimum number of hotdogs needed =", number_of_hotdog_packages_needed)
+print("Minimum number of buns needed =", number_of_hotdog_buns_needed)
+print("Number of hot dogs left over =", number_of_hotdogs_left_over)
+print("Number of hot dogs buns left over =", number_of_hotdog_buns_left_over)
