e24d9d1d254c39a999f4d277bb3370f4aeff0b89
Alice <aliceblack@westernwyoming.edu>
2024-09-16 21:15:32 -0700

    Update main.py

--- Areas-of-Rectangles/main.py
+++ Areas-of-Rectangles/main.py
@@ -1,21 +1,29 @@
-#
-# Name
-# Date
+# Alice J Black
+# 2024SEP16
 # Areas of Rectangles Programming Project
-# COSC 2409 DNT
+# COSC1010
 #
 # Local variables
-
-# Get length A
-
-# Get width A
-
-# Get length B
-
-# Get width B
 
 # Calculate area A
 
 # Calculate area B
 
 # Print area comparison using if-elif-else statements
+
+# Get length A
+length_1 = float(input('Enter length for triangle #1: '))
+# Get width A
+width_1 = float(input('Enter width for triangle #1: '))
+# Get area A
+area_1 = length_1 * width_1
+# Get length B
+length_2 = float(input('Enter length for triangle #2: '))
+# Get width A
+width_2 = float(input('Enter width for triangle #2: '))
+# Get Area 2
+area_2 = length_2 * width_2
+#Calculate areas A and B
+if area_1 > area_2:
+    print('Rectangle #1 has the greatest area at', area_1, 'inches.')
+elif area_2 > area_1:
