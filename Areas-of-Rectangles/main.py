a0dde5fde2239b3c3c7ba7e35f0f78d70407279c
Alice <aliceblack@westernwyoming.edu>
2024-09-19 16:23:19 -0700

    Update main.py

--- Areas-of-Rectangles/main.py
+++ Areas-of-Rectangles/main.py
@@ -13,17 +13,26 @@
 
 # Get length A
 length_1 = float(input('Enter length #1: '))
+
 # Get width A
 width_1 = float(input('Enter width #1: '))
+
 # Get area A
 area_1 = length_1 * width_1
+
 # Get length B
 length_2 = float(input('Enter length #2: '))
+
 # Get width A
 width_2 = float(input('Enter width #2: '))
+
 # Get Area 2
 area_2 = length_2 * width_2
+
 #Calculate areas A and B
 if area_1 > area_2:
-    print('Rectangle #1 has the greatest area at', area_1, 'inches.')
+print('Rectangle #1 has the greatest area at', area_1, 'inches.')
 elif area_2 > area_1:
+print('Rectangle #2 has the greatest area at', area_2, 'inches.')
+elif area_1 == area_2:
+print('Rectangles #1 and #2 have the SAME area at', area_1, 'inches')
