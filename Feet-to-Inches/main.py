dad083666007f2641be8726d6359907672982bd2
Alice <aliceblack@westernwyoming.edu>
2024-09-30 21:18:58 -0700

    Update main.py

--- Feet-to-Inches/main.py
+++ Feet-to-Inches/main.py
@@ -1,7 +1,14 @@
 #
-# Name
-# Date
+# Alice J Black
+# 2024SEP30
 # Feet to Inches Programming Project
-# COSC 2409 DNT
+# COSC1010
 #
-# Use comments liberally throughout the program.
+# Feet to inches calculator
+#
+# Define feet
+feet = float(input("Please enter feet:"))
+# Calculate amount of inches
+inches = feet * 12
+# Display inches
+print(inches, " Inches")
