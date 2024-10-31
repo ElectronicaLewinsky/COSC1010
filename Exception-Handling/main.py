dcd4094e2654ee82b3ebc2df2ce59cf19efef375
Alice <aliceblack@westernwyoming.edu>
2024-10-30 21:29:04 -0600

    Update main.py

--- Exception-Handling/main.py
+++ Exception-Handling/main.py
@@ -1,7 +1,32 @@
 #
-# Name
-# Date
-# Exception Handling Programming Project
-# COSC 2409 DNT
+# Alice J Black
+# 2024OCT30
+# Exception Handling Project
+# COSC 1010
 #
-# Use comments liberally throughout the program. 
+#
+# 
+# Open file
+loop_count = 0
+total = 0
+data = open("numbers.txt","r")
+# Handle IOError 
+try:
+    f = open('numbers.txt', 'r')
+    content = f.read()
+    f.close()
+except IOError:
+    print('File not found.')
+# Read data
+for line in data.readlines():
+    # Calculate average
+    total = total + int(line)
+    loop_count += 1
+avg = total/loop_count
+# Handle ValueError
+try:
+    data = int
+except ValueError:
+    print("Data is invalid.")
+# Display average
+print (avg)
