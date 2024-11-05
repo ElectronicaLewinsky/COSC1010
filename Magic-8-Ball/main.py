5f2b8b91b740e5a8de7afaf8dfeb21eeef691e4a
Alice <aliceblack@westernwyoming.edu>
2024-11-05 00:13:07 -0700

    Update main.py

--- Magic-8-Ball/main.py
+++ Magic-8-Ball/main.py
@@ -1,7 +1,29 @@
 #
-# Name
-# Date
+# Alice J Black
+# 2024NOV5
 # Magic 8 Ball Programming Project
-# COSC 2409 DNT
+# COSC 1010
 #
-# Use comments liberally throughout the program. 
+#
+#
+#
+# Import Random Module
+import random
+# Import Time Module
+import time
+# Define input or abort
+question = input("Ask the magic 8 ball a question (press enter to abort): ") 
+while question:
+    if question == "quit":
+        break
+# Display text and define variables
+    print("Let's see what your future holds")    
+    for i in range(random.randrange(3,10)):
+        print(". ",end=" ")
+        time.sleep(1)
+    print("\nYour answer is")
+# Define result
+    print("Your future is " +(random.choice(open("8_ball_responses.txt","r").read().splitlines())) + " .")
+    question = input("Ask the magic 8 ball a question (press enter to quit): ")
+
+print("byeee")
